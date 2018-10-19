e019396071c18b7a5aca2d14370f4785467d7243
刘超 <363265580@qq.com>
2018-10-19 11:11:38 +0800

    首次提交

---
+++ webapp-api/src/main/java/org/webapp/api/App.java
@@ -0,0 +1,13 @@
+package org.webapp.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
